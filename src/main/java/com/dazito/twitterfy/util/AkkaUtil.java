1206db3ddc51f46e16fc6bbb9c1c1819d56a9001
Pedro <plm@dazito.com>
2017-04-17 21:35:29 +0100

    AkkaUtil and Constant init

---
+++ src/main/java/com/dazito/twitterfy/util/AkkaUtil.java
@@ -0,0 +1,33 @@
+package com.dazito.twitterfy.util;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.routing.FromConfig;
+import com.dazito.twitterfy.actor.ActorSystemContainer;
+
+/**
+ * Created by daz on 16/04/2017.
+ */
+public final class AkkaUtil {
+
+    private AkkaUtil() {}
+
+    public static ActorRef configureRouter(Class actorClass, String actorName, ActorRef awsSns, ActorRef gcPubsub, ActorRef db) {
+        // Create actor system on startup
+        final ActorSystem actorSystem = ActorSystemContainer.getInstance().getActorSystem();
+
+        return actorSystem.actorOf(
+                FromConfig.getInstance().props(Props.create(actorClass, awsSns, gcPubsub, db)), actorName
+        );
+    }
+
+    public static ActorRef configureRouter(Class actorClass, String actorName) {
+        // Create actor system on startup
+        final ActorSystem actorSystem = ActorSystemContainer.getInstance().getActorSystem();
+
+        return actorSystem.actorOf(
+                FromConfig.getInstance().props(Props.create(actorClass)), actorName
+        );
+    }
+}
---
+++ src/main/java/com/dazito/twitterfy/util/Constant.java
@@ -0,0 +1,14 @@
+package com.dazito.twitterfy.util;
+
+/**
+ * Created by daz on 16/04/2017.
+ */
+public final class Constant {
+
+    private Constant() {}
+
+    public static final String TWEET_ROUTER_NAME = "tweet-router";
+    public static final String AWS_SNS_ROUTER_NAME= "aws-sns-router";
+    public static final String GC_PUBSUB_ROUTER_NAME = "gc-pubsub-router";
+    public static final String DATABASE_ROUTER_NAME = "database-router";
+}
